d7e6d5598ec3685253d66fe7d9949c83aaa2f63c
KoljaPl <nikolya.plugin@gmail.com>
2025-01-28 19:55:35 +0100

    Ну что то сделал, дополнил #TODO Well, I did something, added to the #TODO.

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from sympy import symbols, solve, sqrt
+from sympy import symbols, solve, sqrt, Eq
+from math import pow
 
+#TODO сделай плис racionalSOLUTION и дополни
+#def racionalSOLUTION():
+#   print("Ты выбрал рациональные функции.")
+
+#TODO сделай плис LinealSolution и дополни
+#def LinealSolution():
+#   print("Ты выбрал линейные или ниленейные функции.")
+
+def choose_solution():
+    print("Выбирите какой пример хотите решать:\n (1) Примеры работы с рациональными функциеми с квадратным корнем в знаменателе.",
+          "Для ответа напишите 1\n (2) Пример решения линейного или нелинейного неравенства. Для ответа напишите 2")
+    n = int(input())
+    if n == 1:
+        racionalSOLUTION()
+    elif n == 2:
+        LinealSolution()
+    else:
+        print("Ты не нато нажал.")
 inequality = str(input())
+
 # Определяем переменные
 x = symbols('x')
 
-# Задаем неравенство
+# Задаем неравенство 2 / sqrt((x - sqrt(3) * sqrt(3) - 2 * x + 1)) = g(x)
 #inequality = (4 - sqrt(17)) * (3 * x + 5) >= 0
 
 # Решаем неравенство
--- main3.py
+++ main3.py
@@ -1,7 +1,43 @@
-m = int(input())
-p = int(input())
-n = int(input())
-k = m * (p / 100)
-for i in range(n):
-    print(i + 1, m + k)
-    k = k + k
+from sympy import symbols, sqrt, solve, Eq, And
+
+
+def solve_case(inequality_str: str):
+    x = symbols('x')  # Определяем переменную x
+
+    # Преобразуем строку в выражение
+    inequality = eval(inequality_str)
+
+    if isinstance(inequality, (Eq, bool)):
+        # Если это уравнение или логическое выражение, решаем его
+        solution = solve(inequality, x)
+        return solution
+    else:
+        # Если это выражение, проверяем его область определения
+        # Условие 1: знаменатель не равен 0
+        denominator = inequality.as_numer_denom()[1]  # Получаем знаменатель
+        denominator_not_zero = solve(Eq(denominator, 0), x)
+
+        # Условие 2: подкоренное выражение (если есть корень) ≥ 0
+        if denominator.has(sqrt):
+            radicand = denominator.args[0]  # Получаем подкоренное выражение
+            inequality_solution = solve(radicand >= 0, x)
+        else:
+            inequality_solution = None
+
+        return {
+            "Область определения (знаменатель ≠ 0)": denominator_not_zero,
+            "Область определения (подкоренное выражение ≥ 0)": inequality_solution
+        }
+
+
+# Запрашиваем ввод от пользователя
+print(
+    "Введите пример для решения (например, '2 / sqrt((x - sqrt(3))**2 - 2 * x + 1)' или '(4 - sqrt(17)) * (3 * x + 5) >= 0'):")
+inequality_input = input()
+
+# Решаем пример
+try:
+    solution = solve_case(inequality_input)
+    print("Решение:", solution)
+except Exception as e:
+    print("Ошибка при обработке примера:", e)
