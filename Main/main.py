21cdf5b0b1f36e8535fea205d9eb97c8c3369d09
KoljaPl <nikolya.plugin@gmail.com>
2025-01-31 19:05:25 +0100

    Доделал проценты, исправил много всего, мега устал :(

--- Main/main.py
+++ Main/main.py
@@ -4,7 +4,6 @@ from Main.defs.malfunctions.DiffIntegrateSolution import DiffIntegrateSolution
 from Main.defs.DefoltDefs.ArithmeticSolution import ArithmeticSolution
 from Main.defs.DefoltDefs.AlgebraicSolution import AlgebraicSolution
 from Main.defs.DefoltDefs.PercentsSolution import PercentsSolution
-#from Main.defs.DefSolution import ArithmeticSolution, TrigonometricSolution
 #from Main.defs.DefSolution import LogExpandSolution, FractionsPercentsSolution, DiffIntegrateSolution
 
 #Сама главная функция с вызовами подфункций
@@ -12,9 +11,9 @@ def choose_solution():
     print("Выбирите какой пример хотите решать:\n"
     " (1) Для решения арифметических выражений нажмите 1 \n",
     "(2) Для решения сложных алгебраических уравнений нажмите 2 \n",
-    "(3) Для решения тригонометрических уравнений и/или выражений нажмите 3 НЕРАБОТАЕТ\n",
+    "(3) Для решения процентов нажмите 3\n",
     "(4) Для решения логарифмов и экспонентов нажмите 4 НЕРАБОТАЕТ\n",
-    "(5) Для решения процентов нажмите 5 \n",
+    "(5) Для решения тригонометрических уравнений и/или выражений нажмите 3 НЕРАБОТАЕТ \n",
     "(6) Для выбора дифференцирование и интегрирование нажмите 6 НЕРАБОТАЕТ\n",)
     n = int(input("Введи какой тип примера хочешь решать:"))
     if n == 1:
@@ -22,11 +21,11 @@ def choose_solution():
     elif n == 2:
         AlgebraicSolution()
     elif n == 3:
-        TrigonometricSolution()
+        PercentsSolution()
     elif n == 4:
         LogExpandSolution()
     elif n == 5:
-        PercentsSolution()
+        TrigonometricSolution()
     elif n == 6:
         DiffIntegrateSolution()
     else:
