f74c47994f6df6cb79cb67237c5a5f16b2074439
KoljaPl <nikolya.plugin@gmail.com>
2025-02-05 22:21:24 +0100

    Добавил отдел с формулами, доделать надо с помощью класов и ооп

---
+++ Main/defs/Formuls/FormulMath.py
@@ -0,0 +1,3 @@
+
+def FormulMath():
+    print()
--- Main/main.py
+++ Main/main.py
@@ -4,11 +4,14 @@ from Main.defs.malfunctions.DiffIntegrateSolution import DiffIntegrateSolution
 from Main.defs.DefoltDefs.ArithmeticSolution import ArithmeticSolution
 from Main.defs.DefoltDefs.AlgebraicSolution import AlgebraicSolution
 from Main.defs.DefoltDefs.PercentsSolution import PercentsSolution
-#from Main.defs.DefSolution import LogExpandSolution, FractionsPercentsSolution, DiffIntegrateSolution
+from Main.defs.Formuls.FormulMath import FormulMath
+from Main.defs.Formuls.FormulPhisics import FormulPhisic
+# from Main.defs.DefSolution import LogExpandSolution, FractionsPercentsSolution, DiffIntegrateSolution
+
 
 #Сама главная функция с вызовами подфункций
 def choose_solution():
-    print("Выбирите какой пример хотите решать:\n"
+    print("Выберите какой пример хотите решать:\n"
     " (1) Для решения арифметических выражений нажмите 1 \n",
     "(2) Для решения сложных алгебраических уравнений нажмите 2 \n",
     "(3) Для решения процентов нажмите 3\n",
@@ -32,11 +35,35 @@ def choose_solution():
         print("Ты не нато нажал.")
         choose_solution() # Перезапуск функции если пользователь ввел не ту цифру
 
+# Функция выбора
+def choose_what():
+    print("Привет, это программа с универсальными решениями, что ты хочешь решать,"
+          "математику с арифметикой и алгеброй (нажми 1) или посмотреть формулы для математики и физики (нажми 2)?")
+    n = int(input("Что хочешь?"))
+    if n == 1:
+        try:
+            choose_solution()
+        except KeyboardInterrupt:
+            print("\n\nПрограмма завершилась ручным выключением.")
+        except ValueError:
+            print("\n\nТы ввел что-то не то.")
+            choose_solution()
+    elif n == 2:
+        print("Ты выбрал формулы, для чего формулу ты хочешь решать? математика - (1), физика - (2)")
+        n = int(input("Что хочешь?"))
+        if n == 1:
+            FormulPhisic()
+        elif n == 2:
+            FormulMath()
+    else:
+        print("Ты не нато нажал.")
+        choose_what() # Перезапуск функции если пользователь ввел не ту цифру
+
 #Вызов главной функции
 try:
-    choose_solution()
+    choose_what()
 except KeyboardInterrupt:
     print("\n\nПрограмма завершилась ручным выключением.")
 except ValueError:
     print("\n\nТы ввел что-то не то.")
-    choose_solution()
+    choose_what()
